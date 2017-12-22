1397ef09e08d0e6cc8b8cc5aaebd69415e7503d8
Jan Dobes <git@owny.cz>
2017-12-22 16:47:09 +0100

    play with options

--- vmscript.sh
+++ vmscript.sh
@@ -10,7 +10,7 @@ else
 
 # use pulseaudio
 export QEMU_AUDIO_DRV=pa
-export QEMU_PA_SAMPLES=8192
+export QEMU_PA_SAMPLES=1024
 export QEMU_AUDIO_TIMER_PERIOD=99
 export QEMU_PA_SERVER=/run/user/1000/pulse/native
 
@@ -20,7 +20,7 @@ qemu-system-x86_64 \
 -name $vmname,process=$vmname \
 -machine type=q35,accel=kvm \
 -cpu host,kvm=off \
--smp 4,sockets=1,cores=2,threads=2 \
+-smp 8,sockets=1,cores=4,threads=2 \
 -m 4G \
 -balloon none \
 -rtc clock=host,base=localtime \
@@ -29,15 +29,16 @@ qemu-system-x86_64 \
 -serial none \
 -parallel none \
 -soundhw hda \
--usb -device usb-host,hostbus=1,hostaddr=12 \
+-usb -device usb-host,hostbus=1,hostaddr=5 \
+-device usb-host,hostbus=1,hostaddr=4 \
 -device vfio-pci,host=01:00.0,multifunction=on \
 -device vfio-pci,host=01:00.1 \
 -drive if=pflash,format=raw,readonly,file=/usr/share/edk2.git/ovmf-x64/OVMF_CODE-pure-efi.fd \
 -drive if=pflash,format=raw,file=/tmp/my_vars.fd \
 -boot order=dc \
 -drive id=disk0,if=virtio,cache=none,format=raw,file=/home/jan/libvirt-images/windows10vm.img \
--drive file=/home/jan/libvirt-images/Win10_1709_Czech_x64.iso,media=cdrom \
--drive file=/home/jan/libvirt-images/virtio-win-0.1.141_amd64.vfd,if=floppy \
+#-drive file=/home/jan/libvirt-images/Win10_1709_Czech_x64.iso,media=cdrom \
+#-drive file=/home/jan/libvirt-images/virtio-win-0.1.141_amd64.vfd,if=floppy \
 #-net bridge,br=virbr0 -net nic,model=virtio
 #-netdev type=tap,id=net0,ifname=vmtap0,vhost=on \
 #-device virtio-net-pci,netdev=net0,mac=00:16:3e:00:01:01
